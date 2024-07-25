4ab40c6667c83f6c05c7bc2a00f39e4baacee051
loanbasewebsite <loanbasewebsite@gmail.com>
2024-07-25 12:36:05 +1000

    Routing and Sider

---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,22 @@
+import Dropdown from "../components/Dropdown";
+import  { useState } from 'react';
+
+function DropdownPage() {
+    const [selection , setSelection] = useState(null);
+
+    const handleSelect = (selected)=>{
+setSelection(selected);
+    }
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
+  return (
+    <div>
+      <Dropdown options={options} value={selection} onChange={handleSelect}/>
+    </div>
+  );
+}
+
+export default DropdownPage;
